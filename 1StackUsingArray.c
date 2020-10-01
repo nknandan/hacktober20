707cbfdfd6bc5bce849c42b089220142550aa6f9
Sourav Satheesh <56963819+SouravSatheesh@users.noreply.github.com>
2020-10-01 14:17:50 +0530

    Update 1StackUsingArray.c

--- 1StackUsingArray.c
+++ 1StackUsingArray.c
@@ -1,82 +1,59 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
+# include < stdio .h >
+# include < stdbool .h >
+# include < stdlib .h >
+typedef struct {
+int arr [10];
+} Stack ;
+Stack s ;
 int top = -1;
 
-typedef struct {
-    /*
-    The stack should contain an array to hold a maximum of 10 elements.
-    */
-    int arr[10];
-    
-} Stack;
+ void push ( int n ) {
+ if( top < 9) {
+ s . arr [++ top ] = n ;
+ }
+ }
 
-/*
-Initialising the stack, use this stack variable 's' in your functions.
-*/
-Stack s;
+ int pop () {
 
-void push(int n) {
-    /*
-    Push the integer n into the stack.
-    Ignore if the operation is not possible.
-    */
-       
-    if(top<9)
-    {
-        s.arr[++top] = n;
-    }
-}
 
-int pop() {
-    /*
-    Pop the top element in the stack and return that element.
-    Return -1 the operation is not possible.
-    */
-     if(top < 0) { 
-        return -1; 
-    } 
-    else { 
-        int x = s.arr[top--]; 
-        return x; 
-    } 
-}
+ int temp ;
+ if( top == -1) {
+ return -1;
+ }
+ else {
+ temp = s . arr [ top - -];
+ return temp ;
+ }
+ }
+ bool isEmpty () {
+ if( top == -1)
+ return true ;
+ else
+ return false ;
+ }
 
-bool isEmpty() {
-    /*
-    Check if the stack is empty or not. Return true/false.
-    */
-    return (top<0);
-}
+ bool isFull () {
+ if( top == 9)
+ return true ;
+ else
+ return false ;
+ }
 
-bool isFull() {
-    /*
-    Check if the stack is full or not. Return true/false.
-    */
-    return (top >= 9);
-}
-
-int main() {
-    int q, choice, n;
-    scanf("%d", &q);
-    while(q--) {
-        scanf("%d%d", &choice, &n);
-        switch(choice) {
-            case 0: push(n);
-                    break;
-            case 1: printf("%d\n", pop());
-                    break;
-            case 2: printf("%d\n", isEmpty());
-                    break;
-            case 3: printf("%d\n", isFull());
-                    break;
-            // case 4: ;
-            //         Stack temp;
-            //         pop(&temp);
-            //         push(&temp, n);
-            //         break;
-        }
-    }
-    return 0;
-}
+ int main () {
+ int q , choice , n ;
+ scanf ("%d", & q ) ;
+ while (q - -) {
+ scanf ("%d%d", & choice , & n );
+ switch ( choice ) {
+ case 0: push ( n ) ;
+ break ;
+ case 1: printf ("%d\n", pop () ) ;
+ break ;
+ case 2: printf ("%d\n", isEmpty () ) ;
+ break ;
+ case 3: printf ("%d\n", isFull () ) ;
+ break ;
+ }
+ }
+ return 0;
+ }
